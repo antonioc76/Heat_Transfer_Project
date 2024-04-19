255bfd649cfa84574ab9305e8e9f5ef6520c92f0
antonioc76 <antoniochappell@protonmail.com>
2024-04-19 01:23:04 -0500

    adding changes reza requested to backwards_conduction. Removed the radiation

--- backward_conduction.m
+++ backward_conduction.m
@@ -1,7 +1,7 @@
 function T_m_i_plus_1 = backward_conduction(T_m_i, T_m_plus_1_i, k, A1, rho, C_p, delta_x, delta_t)
     A_avg = A1;
     conduction_const = k * A1 / delta_x;
-    tau = rho * C_p * A_avg * delta_x / delta_t;
+    tau = rho * C_p * A_avg * delta_x / (2 * delta_t);
 
     T_m_i_plus_1 = (conduction_const * (T_m_plus_1_i - T_m_i) + tau * T_m_i ) / tau;
 end
